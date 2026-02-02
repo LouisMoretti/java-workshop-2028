419a9926b5a34c50b7855ce0d46f9ee3d38b3ce4
Louis Moretti <louis.moretti@epita.fr>
2026-02-02 20:41:19 +0100

    a

--- mySet/src/main/java/fr/epita/assistants/myset/GenericSet.java
+++ mySet/src/main/java/fr/epita/assistants/myset/GenericSet.java
@@ -1,4 +1,88 @@
 package fr.epita.assistants.myset;
 
-public class GenericSet {
+import java.util.ArrayList;
+
+public class GenericSet<T extends Comparable<T>> {
+    ArrayList<T> base;
+
+    public GenericSet() {
+        this.base = new ArrayList<>();
+    }
+
+    public void insert(T i) {
+        int len = base.size();
+        int j = 0;
+        for (; j < len; j++) {
+            if (base.get(j) == i) return;
+            if (base.get(j).compareTo(i) > 0) break;
+        }
+
+        base.add(j, i);
+    }
+
+    public void remove(T i) {
+        base.remove(i);
+    }
+
+    public boolean has(T i) {
+        return base.contains(i);
+    }
+
+    public boolean isEmpty() {
+        return base.isEmpty();
+    }
+
+    public T min() {
+        return base.getFirst();
+    }
+
+    public T max() {
+        return base.getLast();
+    }
+
+    public int size() {
+        return base.size();
+    }
+
+    public static <T extends Comparable<T>> GenericSet<T> intersection(GenericSet<T> a, GenericSet<T> b) {
+        GenericSet<T> res = new GenericSet<>();
+
+        int size1 = a.size();
+        int size2 = b.size();
+
+        for (int i = 0, j = 0; i < size1 && j < size2; ) {
+            T aFirst = a.base.get(i);
+            T bFirst = b.base.get(j);
+            if (aFirst == bFirst) {
+                res.insert(a.base.get(i));
+                i++;
+                j++;
+            } else if (aFirst.compareTo(bFirst) < 0) {
+                i++;
+                if (i >= size1) break;
+            } else {
+                j++;
+                if (j >= size2) break;
+            }
+
+        }
+
+        return res;
+    }
+
+    public static <T extends Comparable<T>> GenericSet<T> union(GenericSet<T> a, GenericSet<T> b) {
+        GenericSet<T> res = new GenericSet<>();
+
+        int size = a.size();
+        for (int i = 0; i < size; i++) {
+            res.insert(a.base.get(i));
+        }
+
+        size = b.size();
+        for (int i = 0; i < size; i++) {
+            res.insert(b.base.get(i));
+        }
+
+        return res;
+    }
 }
--- mySet/src/main/java/fr/epita/assistants/myset/IntegerSet.java
+++ mySet/src/main/java/fr/epita/assistants/myset/IntegerSet.java
@@ -9,39 +9,44 @@ public class IntegerSet {
         this.base = new ArrayList<>();
     }
 
-    private int findPlace(Integer i) {
-        int min = 0;
-        int max = base.size() - 1;
-        int mid = max / 2;
-
-        while (min < max) {
-            mid = (min + max) / 2;
-            if (i.intValue() == base.get(mid).intValue()) {
-                return mid;
-            } else if (i < base.get(mid)) {
-                max = mid - 1;
-            } else {
-                min = mid + 1;
-            }
-        }
-
-        return mid;
-    }
+//    private int findPlace(Integer i) {
+//        int min = 0;
+//        int max = base.size() - 1;
+//        int mid = max / 2;
+//
+//        while (min < max) {
+//            mid = (min + max) / 2;
+//            if (i.intValue() == base.get(mid).intValue()) {
+//                return mid;
+//            } else if (i < base.get(mid)) {
+//                max = mid - 1;
+//            } else {
+//                min = mid + 1;
+//            }
+//        }
+//
+//        return mid;
+//    }
 
     public void insert(Integer i) {
-        int place = findPlace(i);
-        if (base.size() > place && i == base.get(place))
-            return;
+        int len = base.size();
+        int j = 0;
+        for (; j < len; j++) {
+            if (base.get(j) == i) return;
+            if (base.get(j) > i) break;
+        }
 
-        base.add(place, i);
+        base.add(j, i);
+
+//        int place = findPlace(i);
+//        if (base.size() > place && i == base.get(place))
+//            return;
+//
+//        base.add(place, i);
     }
 
     public void remove(Integer i) {
-        int place = findPlace(i);
-        if (i.intValue() != base.get(place).intValue())
-            return;
-
-        base.remove(place);
+        base.remove(i);
     }
 
     public boolean has(Integer i) {
@@ -70,7 +75,7 @@ public class IntegerSet {
         int size1 = a.size();
         int size2 = b.size();
 
-        for (int i = 0, j = 0; i < size1 && j < size2;) {
+        for (int i = 0, j = 0; i < size1 && j < size2; ) {
             Integer aFirst = a.base.get(i);
             Integer bFirst = b.base.get(j);
             if (aFirst == bFirst) {
