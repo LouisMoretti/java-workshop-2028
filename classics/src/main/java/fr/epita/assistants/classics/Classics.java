4b8ba0f736b5c1a7f3d89cad31ba22f3aeba136f
Louis Moretti <louis.moretti@epita.fr>
2026-02-02 11:41:58 +0100

    Classics fix 4

--- classics/src/main/java/fr/epita/assistants/classics/Classics.java
+++ classics/src/main/java/fr/epita/assistants/classics/Classics.java
@@ -26,7 +26,14 @@ public class Classics {
         if (n < 0) return -1;
         if (n == 0) return 0;
         if (n <= 2) return 1;
-        return tribonacci(n - 3) + tribonacci(n - 2) + tribonacci(n - 1);
+        int[] array = new int[n + 1];
+        array[0] = 0;
+        array[1] = 1;
+        array[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            array[i] = array[i-1] + array[i-2] + array[i-3];
+        }
+        return array[n];
     }
 
     private static boolean isSpace(char c){
