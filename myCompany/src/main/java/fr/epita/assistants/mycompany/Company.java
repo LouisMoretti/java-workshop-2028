42197be67934e9ae5e8a17e3a136ca51297e9760
Louis Moretti <louis.moretti@epita.fr>
2026-02-02 12:57:40 +0100

    myCompany archi

---
+++ myCompany/src/main/java/fr/epita/assistants/mycompany/Company.java
@@ -0,0 +1,4 @@
+package fr.epita.assistants.mycompany;
+
+public class Company {
+}
---
+++ myCompany/src/main/java/fr/epita/assistants/mycompany/Employee.java
@@ -0,0 +1,23 @@
+package fr.epita.assistants.mycompany;
+
+public abstract class Employee {
+    private String firstName;
+    private String lastName;
+
+    public Employee(String lastName, String firstName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String toString() {
+        return firstName + ' ' + lastName;
+    }
+}
