cd08e4d6558cb08123db104f603a3c06dcb8c79b
Louis Moretti <louis.moretti@epita.fr>
2026-02-03 21:23:24 +0100

    arxhi test1

---
+++ test1/src/test/java/fr/epita/assistants/maths/MatrixTests.java
@@ -0,0 +1,12 @@
+package fr.epita.assistants.maths;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class MatrixTests {
+
+    @Test
+    void testMatrix1() {
+
+    }
+}
