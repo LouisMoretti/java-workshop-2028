7a795f51b93a8a0d1df10a1945ce6c2ca87179b5
Louis Moretti <louis.moretti@epita.fr>
2026-02-04 20:53:38 +0100

    test1 2

--- test1/src/test/java/fr/epita/assistants/maths/MatrixTests.java
+++ test1/src/test/java/fr/epita/assistants/maths/MatrixTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MatrixTests {
 
@@ -40,6 +41,24 @@ public class MatrixTests {
 
         Matrix matrix = new Matrix(rawMatix);
         assert (matrix.getMatrix() == rawMatix);
+    }
+
+    @Test
+    void testMatrixConstructor5() {
+        int[][] notAMatix = {
+                {1, 1, 1, 42, 1, 1, 1},
+                {1}};
+
+        Matrix matrix = new Matrix(notAMatix);
+        assert (matrix.getMatrix() == notAMatix);
+    }
+
+    @Test
+    void testMatrixConstructor6() {
+        int[][] notAMatix = {};
+
+        Matrix matrix = new Matrix(notAMatix);
+        assert (matrix.getMatrix().length == 0);
     }
 
     @Test
@@ -107,6 +126,16 @@ public class MatrixTests {
     }
 
     @Test
+    void testMatrixEquals6() {
+        int[][] rawMatix1 = {{1}};
+
+        Matrix matrix1 = new Matrix(rawMatix1);
+        assertThrows(ClassCastException.class, () -> {
+            matrix1.equals("matrix2");
+        });
+    }
+
+    @Test
     void testMatrixMultiply1() {
         int[][] rawMatix = {
                 {1, 1},
@@ -121,7 +150,7 @@ public class MatrixTests {
         Matrix matrix1 = new Matrix(rawMatix);
         Matrix matrix2 = new Matrix(rawMatix);
 
-        assertArrayEquals (matrix1.multiply(matrix2).getMatrix(), result);
+        assertArrayEquals(matrix1.multiply(matrix2).getMatrix(), result);
     }
 
     @Test
@@ -139,6 +168,27 @@ public class MatrixTests {
         Matrix matrix1 = new Matrix(rawMatix);
         Matrix matrix2 = new Matrix(indentity);
 
-        assertArrayEquals (matrix1.multiply(matrix2).getMatrix(), rawMatix);
+        assertArrayEquals(matrix1.multiply(matrix2).getMatrix(), rawMatix);
+    }
+
+    @Test
+    void testMatrixMultiply3() {
+        int[][] rawMatix = {
+                {42, 42},
+                {42, 42}
+        };
+
+        int[][] indentity = {
+                {1, 0, 1},
+                {0, 1, 0},
+                {1, 0, 1}
+        };
+
+        Matrix matrix1 = new Matrix(rawMatix);
+        Matrix matrix2 = new Matrix(indentity);
+
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> {
+            matrix1.multiply(matrix2);
+        });
     }
 }
