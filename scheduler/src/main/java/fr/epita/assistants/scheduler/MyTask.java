f358be66123cb903d6367174def08021cd3e1c17
Louis Moretti <louis.moretti@epita.fr>
2026-02-05 14:34:27 +0100

    scheduler

--- scheduler/src/main/java/fr/epita/assistants/scheduler/MyTask.java
+++ scheduler/src/main/java/fr/epita/assistants/scheduler/MyTask.java
@@ -6,32 +6,35 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class MyTask<RETURN_TYPE> implements Task<RETURN_TYPE> {
-    
-    /* FIXME */
 
-    /* FIXME */
+    CompletableFuture<RETURN_TYPE> myFuture;
+
+    private MyTask(CompletableFuture<RETURN_TYPE> myFuture) {
+        this.myFuture = myFuture;
+    }
 
     public static <RETURN_TYPE> Task<RETURN_TYPE> of(Supplier<RETURN_TYPE> actionSupplier) {
-        throw new UnsupportedOperationException("FIXME");
+        return new MyTask<>(CompletableFuture.supplyAsync(actionSupplier));
     }
 
     @Override
     public CompletableFuture<RETURN_TYPE> build() {
-        throw new UnsupportedOperationException("FIXME");
+        return myFuture;
     }
 
     @Override
     public Task<RETURN_TYPE> onErrorRecoverWith(Function<Throwable, RETURN_TYPE> recoveryFunction) {
-        throw new UnsupportedOperationException("FIXME");
+        return new MyTask<>(myFuture.exceptionally(recoveryFunction));
     }
 
     @Override
     public <NEW_RETURN_TYPE> Task<NEW_RETURN_TYPE> andThenDo(Function<RETURN_TYPE, NEW_RETURN_TYPE> action) {
-        throw new UnsupportedOperationException("FIXME");
+        return new MyTask<>(myFuture.thenApply(action));
     }
 
     @Override
     public Task<RETURN_TYPE> andThenWait(long number, TimeUnit timeUnit) {
-        throw new UnsupportedOperationException("FIXME");
+        return new MyTask<>(myFuture.thenApplyAsync(Function.identity(), CompletableFuture.delayedExecutor(number,
+                timeUnit)));
     }
 }
