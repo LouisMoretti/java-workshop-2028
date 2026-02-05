fd2a4dad82e08468bc30292361f0dddba3656a51
Louis Moretti <louis.moretti@epita.fr>
2026-02-05 22:17:43 +0100

    pizza

--- pizzaStreams/src/main/java/fr/epita/assistants/pizzastreams/PizzaStreams.java
+++ pizzaStreams/src/main/java/fr/epita/assistants/pizzastreams/PizzaStreams.java
@@ -1,6 +1,7 @@
 package fr.epita.assistants.pizzastreams;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import fr.epita.assistants.pizzastreams.Topping.*;
@@ -10,8 +11,9 @@ public class PizzaStreams {
      * @return The sum of the prices of all the pizzas in the stream
      */
     public static Integer getTotalPrice(Stream<Pizza> pizzaStream) {
-        /* FIXME */
-        throw new NoClassDefFoundError();
+        return pizzaStream
+                .mapToInt(Pizza::getPrice)
+                .sum();
     }
 
     /**
@@ -19,40 +21,50 @@ public class PizzaStreams {
      * double NaN if the stream is empty
      */
     public static Double getAveragePrice(Stream<Pizza> pizzaStream) {
-        /* FIXME */
-        throw new NoClassDefFoundError();
+        return pizzaStream
+                .mapToInt(Pizza::getPrice)
+                .average()
+                .orElse(Double.NaN);
     }
 
     /**
      * @return Names of the pizzas, sorted by price in ascending order
      */
     public static List<String> sortByPrice(Stream<Pizza> pizzaStream) {
-        /* FIXME */
-        throw new NoClassDefFoundError();
+        return pizzaStream
+                .sorted((pizza1, pizza2) -> pizza1.getPrice().compareTo(pizza2.getPrice()))
+                .map(Pizza::getName)
+                .toList();
     }
 
     /**
      * @return The Pizza object that has the lowest price, or null by default
      */
     public static Pizza getCheapest(Stream<Pizza> pizzaStream) {
-        /* FIXME */
-        throw new NoClassDefFoundError();
+        return pizzaStream
+                .min((pizza1, pizza2) -> pizza1.getPrice().compareTo(pizza2.getPrice()))
+                .orElse(null);
     }
 
     /**
      * @return Names of the pizzas with meat (Protein)
      */
     public static List<String> getCarnivorous(Stream<Pizza> pizzaStream) {
-        /* FIXME */
-        throw new NoClassDefFoundError();
+        return pizzaStream
+                .filter(pizza -> pizza.getTopping().getProtein().isPresent())
+                .map(Pizza::getName)
+                .toList();
     }
 
     /**
      * @return Names of the pizzas with at least one Vegetable and no Proteins
      */
     public static List<String> getVeggies(Stream<Pizza> pizzaStream) {
-        /* FIXME */
-        throw new NoClassDefFoundError();
+        return pizzaStream
+                .filter(pizza -> pizza.getTopping().getProtein().isEmpty()
+                        && !pizza.getTopping().getVegetableList().isEmpty())
+                .map(Pizza::getName)
+                .toList();
     }
 
     /**
@@ -60,7 +72,9 @@ public class PizzaStreams {
      * and mozzarella (italian pizza criteria), false otherwise
      */
     public static boolean areAllNatureItalians(Stream<Pizza> pizzaStream) {
-        /* FIXME */
-        throw new NoClassDefFoundError();
+        return pizzaStream
+                .allMatch(pizza -> pizza.getDough() == Dough.NATURE
+                        && pizza.getTopping().getSauce() == Sauce.TOMATO
+                        && pizza.getTopping().getCheese() == Cheese.MOZZARELLA);
     }
 }
