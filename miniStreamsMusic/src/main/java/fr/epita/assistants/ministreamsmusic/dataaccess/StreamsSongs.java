54ad774f7438fec707ea6eccf76260841630129d
Louis Moretti <louis.moretti@epita.fr>
2026-02-05 14:38:38 +0100

    miniStreamMusic

---
+++ miniStreamsMusic/src/main/java/fr/epita/assistants/ministreamsmusic/dataaccess/StreamsSongs.java
@@ -0,0 +1,35 @@
+package fr.epita.assistants.ministreamsmusic.dataaccess;
+
+import fr.epita.assistants.ministreamsmusic.data.Artist;
+import fr.epita.assistants.ministreamsmusic.data.Song;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamsSongs {
+    public static List<String> getOlderArtists(List<Song> songs) {
+        return songs.stream()
+                .map(Song::getArtist)
+                .filter(artist -> artist.getAge() >= 30)
+                .map(Artist::getSurname)
+                .distinct()
+                .limit(10)
+                .collect(Collectors.toList());
+    }
+
+    public static Integer getSumAges(List<Song> songs) {
+        return songs.stream()
+                .map(Song::getArtist)
+                .filter(artist -> artist.getAge() >= 20)
+                .mapToInt(Artist::getAge)
+                .sum();
+    }
+
+    public static List<String> getMusics(List<Song> songs) {
+        return songs.stream()
+                .filter(song -> song.getArtist().getName().toLowerCase().contains("an"))
+                .map(Song::getName)
+                .limit(10)
+                .collect(Collectors.toList());
+    }
+}
