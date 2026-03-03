8e6710dc3e986faefd2c23b675240c775e785085
Louis Moretti <louis.moretti@epita.fr>
2026-03-03 19:03:07 +0100

    travel fix

--- travel/src/main/java/fr/epita/assistants/travel/Country.java
+++ travel/src/main/java/fr/epita/assistants/travel/Country.java
@@ -12,30 +12,30 @@ import java.util.Map;
 import static java.lang.Integer.parseInt;
 
 public class Country {
-    String countryName_;
-    ZoneId zoneId_;
-    String inputFile_;
+    String countryName;
+    ZoneId zoneId;
+    String inputFile;
+    Map<String, Integer> travelTimes;
 
     public Country(String countryName, String countryZone, String inputFilePath) {
-        countryName_ = countryName;
-        zoneId_ = ZoneId.of(countryZone);
-        inputFile_ = inputFilePath;
+        this.countryName = countryName;
+        this.zoneId = ZoneId.of(countryZone);
+        this.inputFile = inputFilePath;
+        this.travelTimes = new HashMap<>();
     }
 
     public Map<String, Integer> initTravelTimes(String inputFilePath) throws IOException {
-        Map<String, Integer> res = new HashMap<>();
-
-        CSVReader csv = new CSVReader(new FileReader(inputFile_), ',');
+        CSVReader csv = new CSVReader(new FileReader(inputFile), ',');
 
         String[] line = null;
         while ((line = csv.readNext()) != null) {
-            if (line[0].equals(countryName_))
-                res.put(line[1], parseInt(line[2]));
+            if (line[0].equals(countryName))
+                travelTimes.put(line[1], parseInt(line[2]));
 
-            if (line[1].equals(countryName_))
-                res.put(line[0], parseInt(line[2]));
+            if (line[1].equals(countryName))
+                travelTimes.put(line[0], parseInt(line[2]));
         }
 
-        return res;
+        return travelTimes;
     }
 }
